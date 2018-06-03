267536b2d23ccb7219acf2ac2635ad471503ae33
Shiva Kanth <shivakanth.sujit@gmail.com>
2018-06-03 19:51:45 +0530

    Clean up UI, but only slightly

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 var canvas = document.getElementById("canvas");
 var ctx = canvas.getContext("2d");
+var pause = document.getElementById("pause");
+var reset = document.getElementById("reset");
+var button ;
+var isPaused = false;
+
+
 var inter;
 ctx.translate(0,canvas.height);
 ctx.scale(1,-1);
@@ -18,13 +24,14 @@ var t0 = [0, 0];
 var playerNo = 0;
 var wEdge = [canvas.width-20-80, 100];
 var hEdge = [70, 70];
+var isOver;
 
 drawTank();
 drawMount();
 drawB(0);
 drawB(1);
 var cr = ["#0095DD", "#F5B041"]
-
+alert("Press Screen to decide direction of shot. Player 1 goes first");
 function drawBall(bno) {
 	if(!isRunning[bno])
 	{
@@ -35,31 +42,24 @@ function drawBall(bno) {
 }
 
 function draw() {
+	
     ctx.fillStyle = "#F5DFDA";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+    var sMessage = "Player 1: " + score[0] + "\tPlayer 2: " + score[1]; 
+    document.getElementById("demo").innerHTML = sMessage;
     drawBall(playerNo);
     drawTank();
     drawMount();
-    
+    isOver = false;
     if( ((playerNo == 0 && x[playerNo] + dx[playerNo] > wEdge[playerNo]) || (playerNo == 1 && x[playerNo] + dx[playerNo] < wEdge[playerNo] )) && y[playerNo] + dy[playerNo] < 70 )
     	{
     		score[playerNo]++;
-    		var sMessage = "Player 1: " + score[0] + " Player 2: " + score[1]; 
+    		var sMessage = "Player 1: " + score[0] + ",\t Player 2: " + score[1]; 
     		document.getElementById("demo").innerHTML = sMessage;
     		alert(sMessage);
         	resetScreen();
-    		if(noOfShots[1] == 0)
-			{
-				if(score[0] > score[1])
-					alert("GAME OVER: Player 1 Wins");
-				else if(score[0] < score[1])
-					alert("GAME OVER: Player 2 Wins");
-				else
-					alert("GAME OVER: Tie");
-       		 	resetScreen();
-    			noOfShots[0] = noOfShots[1] = 3;
-    			score[0] = score[1] = 0;
-			}
+        	isOver = true;
+    		
     		
     	}
     if(x[playerNo] + dx[playerNo] > canvas.width-ballRadius || x[playerNo] + dx[playerNo] < ballRadius) 
@@ -70,19 +70,7 @@ function draw() {
     {
         alert("BALL LOST");
        	resetScreen();
-    	if(noOfShots[1] == 0)
-			{
-				if(score[0] > score[1])
-					alert("GAME OVER: Player 1 Wins");
-				else if(score[0] < score[1])
-					alert("GAME OVER: Player 2 Wins");
-				else
-					alert("GAME OVER: Tie");
-       		 	resetScreen();
-    			noOfShots[0] = noOfShots[1] = 3;
-    			score[0] = score[1] = 0;
-			}
-
+       	isOver = true;
     }
     else if(y[playerNo] + dy[playerNo] > canvas.height-ballRadius) {
  	          dy[playerNo] = -dy[playerNo];
@@ -97,39 +85,57 @@ function draw() {
 	    dy[playerNo] = Math.floor( uy[playerNo] - 9.81*t );
 	    checkImpact();
 	}
-	var coords1 = "X coords: " + x[0] + " " + x[1] + ", Y coords: " + y[0] + " " + y[1] + " , Score: " + score[0] + " " + score[1] + " , Shots Left: " + noOfShots[0] + " " + noOfShots[1];
-    document.getElementById("demo").innerHTML = coords1;
+	
+
+    if(noOfShots[1] == 0 && isOver == true)
+			{
+				if(score[0] > score[1])
+					alert("GAME OVER: Player 1 Wins");
+				else if(score[0] < score[1])
+					alert("GAME OVER: Player 2 Wins");
+				else
+					alert("GAME OVER: Tie");
+       		 	resetScreen();
+    			noOfShots[0] = noOfShots[1] = 3;
+    			score[0] = score[1] = 0;
+    			resetScreen();
+			}
+	//var coords1 = "X coords: " + x[0] + " " + x[1] + ", Y coords: " + y[0] + " " + y[1] + " , Score: " + score[0] + " " + score[1] + " , Shots Left: " + noOfShots[0] + " " + noOfShots[1];
+    //document.getElementById("demo").innerHTML = coords1;
 }
 
 canvas.addEventListener('click', function(e) 
 {
-	noOfShots[playerNo]--;
-	var x1 = e.clientX;
-	var y1 = e.clientY;
-	var x0 = [68, 8 + canvas.width-20-40];
-	var y0 = 477;
-	if(playerNo == 0)
-		var angleRad = Math.atan((y0-y1)/(x1-x0[playerNo]));
-	else
-		var angleRad = Math.atan(-1 * (y0-y1)/(x1-x0[playerNo]));
-	ux[playerNo] = Math.floor(v * Math.cos(angleRad));
-	uy[playerNo] = Math.floor(v * Math.sin(angleRad));
-	if(playerNo == 0)
-		dx[playerNo] = ux[playerNo];
-	else
-		dx[playerNo] = -1 * ux[playerNo];
-	dy[playerNo] = uy[playerNo];
-	var coords = "X coords: " + x1 + ", Y coords: " + y1 + "Angle: " + angleRad*180/Math.PI;
-    document.getElementById("cl").innerHTML = coords;
-	inter = setInterval(draw, 10);
+	if(!isRunning[0] && !isRunning[1])
+	{
+		noOfShots[playerNo]--;
+		var x1 = e.clientX;
+		var y1 = e.clientY;
+		var x0 = [68, 8 + canvas.width-20-40];
+		var y0 = 477;
+		if(playerNo == 0)
+			var angleRad = Math.atan((y0-y1)/(x1-x0[playerNo]));
+		else
+			var angleRad = Math.atan(-1 * (y0-y1)/(x1-x0[playerNo]));
+		ux[playerNo] = Math.floor(v * Math.cos(angleRad));
+		uy[playerNo] = Math.floor(v * Math.sin(angleRad));
+		if(playerNo == 0)
+			dx[playerNo] = ux[playerNo];
+		else
+			dx[playerNo] = -1 * ux[playerNo];
+		dy[playerNo] = uy[playerNo];
+		//var coords = "X coords: " + x1 + ", Y coords: " + y1 + "Angle: " + angleRad*180/Math.PI;
+	    //document.getElementById("cl").innerHTML = coords;
+		inter = setInterval(draw, 10);
+	}
 });
 
 canvas.addEventListener('mousemove', function(e) 
 {
     var x11 = e.clientX;
     var y11 = e.clientY;
-    var coords11 = "X coords: " + x11 + ", Y coords: " + y11 ;
-    document.getElementById("coord").innerHTML = coords11;
+    //var coords11 = "X coords: " + x11 + ", Y coords: " + y11 ;
+    //document.getElementById("coord").innerHTML = coords11;
 });
 
 function drawTank()
@@ -184,7 +190,8 @@ function checkImpact()
 	if(ctx.isPointInPath(x[playerNo]+8,canvas.height-y[playerNo]))
 	{
 		alert("NO POINT");
-       	resetScreen();	
+       	resetScreen();
+       	isOver = true;	
 	}
 }
 
@@ -215,3 +222,4 @@ function drawB(col)
 	ctx.fill();
 	ctx.closePath();
 }
+
